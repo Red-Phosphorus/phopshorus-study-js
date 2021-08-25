952f1eaf71784388b221293d20d8858911f31292
Phosphorus <necromeister112@gmail.com>
2021-08-25 08:47:35 +0400

    Interaction functions

---
+++ Chapter1/2-6.js
@@ -0,0 +1,7 @@
+
+
+let timeToSleep = prompt("How many hours do you wanna sleep?", "Leave me alone");
+alert(`You sleep ${timeToSleep} hours`);
+confirm("Have you rested? We can go for a walk");
+
+
