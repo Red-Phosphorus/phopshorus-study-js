c455233b424967a38adfd357578119ab6a6b4e81
Phosphorus <necromeister112@gmail.com>
2021-08-25 08:45:36 +0400

    Variables

---
+++ Chapter1/2-4.js
@@ -0,0 +1,11 @@
+let name = "John";
+let sirname = "Marston";
+let age = 30;
+
+let name = "John"; let sirname = "Marston"; let age = 30;
+
+let name = "John",
+    sirname = "Marston",
+    age = 30;
+
+alert(name, sirname, age);
